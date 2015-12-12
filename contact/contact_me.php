62fda8471514640fb9d378a0a95f9e9cb434f9ae
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-12-11 21:46:04 -0800

    email is on a delay, hampers repeatability

--- contact/contact_me.php
+++ contact/contact_me.php
@@ -52,17 +52,29 @@ header-img: "img/the-strategy-1080527_960_720.jpg"
 </form>
 
 <?php
-
-    $name    = stripslashes(trim($_POST['name']));
-    $email   = stripslashes(trim($_POST['email']));
-    $message = stripslashes(trim($_POST['message']));
-    $human   = stripslashes(trim($_POST['human']));
-
-    $to = 'sschadwick@gmail.com';
-    $email_subject = "Website Contact Form:  " + $name;
-    $email_body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: " + $name + "\n\nEmail: " + $email_address + "\n\nPhone: " + $phone + "\n\nMessage:\n" + $message;
-    $headers = 'From: noreply@stevenschadwick.com' . "\r\n" .
-    'Reply-To: noreply@stevenschadwick.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-    mail($to, $email_subject, $email_body, $headers);
+// Check for empty fields
+if(empty($_POST['name'])        ||
+   empty($_POST['email'])       ||
+   empty($_POST['message']) ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+    echo "No arguments Provided!";
+    return false;
+   }
+    
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+    
+// Create the email and send the message
+$to = 'sschadwick@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+if ($_POST['human'] == 5) {
+    mail($to,$email_subject,$email_body,$headers);
+    return true;            
+}
 ?>
