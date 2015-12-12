93f109e4957ebbf027971fbbdef02863da3c13c0
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-12-11 21:10:17 -0800

    more php debug

--- contact/contact_me.php
+++ contact/contact_me.php
@@ -53,18 +53,20 @@ header-img: "img/the-strategy-1080527_960_720.jpg"
 
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-    
-$to = 'sschadwick@gmail.com';
-$email_subject = "Website Contact Form: $name";
+if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['human'] == '5') {
+    $name    = stripslashes(trim($_POST['name']));
+    $email   = stripslashes(trim($_POST['email']));
+    $message = stripslashes(trim($_POST['message']));
+}
+// Create the email and send the message
+$to = 'sschadwick@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
 
 $email_body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: " + $name + "\n\nEmail: " + $email_address + "\n\nPhone: " + $phone + "\n\nMessage:\n" + $message;
 
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-$headers .= 'From: noreply@stevenschadwick.com' . "\r\n";
+$headers = "From: noreply@stevenschadwick.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email"; 
+mail($to, $email_subject, $email_body, $headers);
+return true;   
 
-if(mail($to, $email_subject, $email_body, $headers)
 ?>
