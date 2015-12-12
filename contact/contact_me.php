288c32b41913e6ea39ebfbb99a9078446310aca5
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-12-11 18:33:31 -0800

    more changes

---
+++ contact/contact_me.php
@@ -0,0 +1,44 @@
+<?php
+    if (isset($_POST['message'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $human = intval($_POST['human']);
+        $from = 'noreply@stevenschadwick.com'; 
+        $to = 'sschadwick@stevenschadwick.com'; 
+        $subject = 'Message from Contact Demo ';
+        
+        $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+ 
+        // Check if name has been entered
+        if (!$_POST['name']) {
+            $errName = 'Please enter your name';
+        }
+        
+        // Check if email has been entered and is valid
+        if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errEmail = 'Please enter a valid email address';
+        }
+        
+        //Check if message has been entered
+        if (!$_POST['message']) {
+            $errMessage = 'Please enter your message';
+        }
+        //Check if simple anti-bot test is correct
+        if ($human !== 5) {
+            $errHuman = 'Your anti-spam is incorrect';
+        }
+ 
+// If there are no errors, send the email
+        if ($_POST['submit'] && $human === 5) {
+            if (mail ($to, $subject, $body, $from)) {
+                $result = true;
+                echo '<p>Message sent!</p>'
+            } else {
+                $result = false;
+                echo '<p>Something went wrong</p>'
+            }
+        }
+    }
+?>
+
