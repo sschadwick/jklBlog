254d1b560147cb274cb424e91839b498ce5c9239
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-12-13 12:48:40 -0800

    cleaning up template

--- contact/contact_me.php
+++ contact/contact_me.php
@@ -14,12 +14,13 @@ $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
     
-// Create the email and send the message
-$to = 'sschadwick@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@stevenschadwick.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'sschadwick@gmail.com';
+$email_subject = "Website Contact:  $name";
+$email_body = "Name: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@stevenschadwick.com\n";
 $headers .= "Reply-To: $email_address"; 
-mail($to,$email_subject,$email_body,$headers);
-return true;            
+if(mail($to,$email_subject,$email_body,$headers)) {
+    return true;
+}
+return false;            
 ?>
